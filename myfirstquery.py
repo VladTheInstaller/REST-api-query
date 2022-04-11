0a58a6eacd902e574c6c1ce632a4b48b19a66086
Vlad Mott <vlad@topowar.com>
2022-04-11 14:00:58 -0700

    ver 0.2

--- myfirstquery.py
+++ myfirstquery.py
@@ -1,4 +1,4 @@
-# ver 0.1
+# ver 0.2
 # vlad note: to install the Requests module:
 #  python -m pip install requests
 
@@ -11,10 +11,15 @@ results = requests.get('https://api.weather.gov/gridpoints/TOP/31,80/forecast')
 if results.ok:
     print('response status: 200 ok')
     #print((results.text))
+    jData = json.loads(results.content)
+
+    print(f'The JSON response contains {len(jData)} properties:')
+    for key in jData:
+        print(f'{key} : {jData[key]}')
 
 
 else:
     print(f'something went wrong with the REST request. response status: {results.status_code}')
-    #print(results.status_code)
+
 
 
