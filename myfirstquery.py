3c8f0ebfb685620be708d45d6d2cf6394a9ef5bb
Vlad Mott <vlad@topowar.com>
2022-04-11 14:27:42 -0700

    v 0.6 now with reponse header info

--- myfirstquery.py
+++ myfirstquery.py
@@ -1,4 +1,4 @@
-# ver 0.5
+# ver 0.6
 # vlad note: to install the Requests module:
 #  python -m pip install requests
 
@@ -10,19 +10,20 @@ apiURL = 'https://swapi.dev/api/'
 #apiURL = 'https://api.weather.gov/gridpoints/TOP/31,80/forecast'
 
 print(f'sending GET request to API URL {apiURL}')
-results = requests.get(apiURL)
+response = requests.get(apiURL)
 
-if results.ok:
+if response.ok:
     print('response status: 200 ok')
+    print(f'Content Type from response header: {response.headers["Content-Type"]}')
 
     # load the returned payload into JSON
-    jData = json.loads(results.content)
+    jData = json.loads(response.content)
 
     print(f'The JSON response contains {len(jData)} properties:')
 
-    #print out the JSON results in a pretty indented format
+    #print out the JSON response in a pretty indented format
     json_formatted_str = json.dumps(jData, indent=2)
     print(json_formatted_str)
 
 else:
-    print(f'something went wrong with the REST request. response status: {results.status_code}')
+    print(f'something went wrong with the REST request. response status: {response.status_code}')
